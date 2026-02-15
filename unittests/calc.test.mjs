933fced33d9b7296620de681fab3b7fb1896981c
petersem <matthew.petersen.68@gmail.com>
2026-02-15 13:01:34 +1000

    test

---
+++ unittests/calc.test.mjs
@@ -0,0 +1,11 @@
+// import { add, mult } from "./src/functions/calc.mjs";
+
+test('sdsdd'), () {
+    expect(1+1).toBe(2);
+}
+
+// test('adds 4 + 5 to equal 9', () => {
+//     expect(add(4,5)).toBe(9);
+// });
+
+
