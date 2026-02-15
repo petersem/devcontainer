237aaf6293837bf9d26cb5fffd4159bbfa6afac3
petersem <matthew.petersem.68@gmail.com>
2026-02-16 09:17:21 +1000

    added unit tests and fix for jest module error

--- tests/calc.test.mjs
+++ tests/calc.test.mjs
@@ -1,11 +1,36 @@
-// import { add, mult } from "./src/functions/calc.mjs";
+import { add, mult } from "./../src/utils/calc.mjs"
 
-test('sdsdd'), () {
-    expect(1+1).toBe(2);
-}
+describe('add function', () => {
+  test('adds two positive numbers', () => {
+    expect(add(2, 3)).toBe(5);
+  });
 
-// test('adds 4 + 5 to equal 9', () => {
-//     expect(add(4,5)).toBe(9);
-// });
+  test('adds two negative numbers', () => {
+    expect(add(-2, -3)).toBe(-5);
+  });
+
+  test('throws error if arguments are not numbers', () => {
+    expect(() => add(2, '3')).toThrow('Both arguments must be numbers');
+  });
+});
+
+describe('mult function', () => {
+  test('multiplies two positive numbers', () => {
+    expect(mult(2, 3)).toBe(6);
+  });
+
+  test('multiplies two negative numbers', () => {
+    expect(mult(-2, -3)).toBe(6);
+  });
+
+  test('multiplies one positive and one negative number', () => {
+    expect(mult(2, -3)).toBe(6);  // deliberately fail here!!
+  });
+
+
+  test('throws error if arguments are not numbers', () => {
+    expect(() => mult(2, '3')).toThrow('Both arguments must be numbers');
+  });
+});
 
 
