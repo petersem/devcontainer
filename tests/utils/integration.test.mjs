831b836de489799346197d56b005d548ef2e4496
petersem <matthew.petersem.68@gmail.com>
2026-04-17 17:14:36 +1000

    chore: run integration tests

---
+++ tests/utils/integration.test.mjs
@@ -0,0 +1,18 @@
+import { add, mult, sub } from "../../src/utils/calc.mjs"
+
+let y = 0;
+
+beforeAll(() => {
+    y = 9;
+});
+
+afterAll(() => {
+    y = 0;
+});
+
+describe('Setup before testing', () => {
+    test('did setup run', () => {
+        expect(y).toBe(9);
+    });
+});
+
